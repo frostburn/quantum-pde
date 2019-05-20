a98a640596909ecbdf8a7a18deabeadae05b2700
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2019-05-20 22:27:03 +0300

    Make rendering parallel with threads
    Add custom visualization options for the episodes

--- render_all.py
+++ render_all.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import tempfile
 import shutil
+from threading import Thread
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -9,30 +10,30 @@ from lattice import RESOLUTIONS
 
 episodes = [
     ["text", "Quantum mechanics explained\nusing an actual wave function", 30],
-    ["schrodinger2D", "harmonic_potential", 30],
-    ["schrodinger2D", "static_gaussian", 30, True],
+    ["schrodinger2D", "harmonic_potential", 30, {'contrast': 3.0}],
+    ["schrodinger2D", "static_gaussian", 30, {'hide_phase': True, 'contrast': 4.0}],
     ["copenhagen", "static_gaussian", 30],
     ["schrodinger2D", "static_gaussian", 30],
     ["classical_particle", "superposition", 30],
     ["schrodinger2D", "gaussian_superposition", 30],
     ["copenhagen", "gaussian_superposition", 30],
-    ["schrodinger2D", "colliding_gaussians", 30],
+    ["schrodinger2D", "colliding_gaussians", 30, {'contrast': 6.0}],
     ["classical_particle", "colliding_superposition", 30],
     ["schrodinger2D", "tunneling_slow", 30],
     ["schrodinger2D", "tunneling_fast", 30],
     ["schrodinger2D", "tunneling", 30],
     ["classical_particle", "tunneling", 30],
-    ["schrodinger2D", "single_slit", 30],
-    ["schrodinger2D", "double_slit", 30],
-    ["classical_particle", "double_slit", 30],
+    ["schrodinger2D", "single_slit", 30, {'contrast': 40.0}],
+    ["schrodinger2D", "double_slit", 30, {'contrast': 40.0}],
+    ["classical_particle", "double_slit", 30, {'sampling_multiplier': 10.0}],
     ["classical_particle", "square_measurement", 30],
     ["classical_particle", "square_measurement_inverted", 30],
-    ["schrodinger2D", "gaussian_measured_inverted", 30],
-    ["schrodinger2D", "gaussian_measured", 30],
-    ["schrodinger2D", "double_slit_measured", 30],
-    ["schrodinger2D", "convex_mirror", 30],
+    ["schrodinger2D", "gaussian_measured_inverted", 30, {'contrast': 4.0}],
+    ["schrodinger2D", "gaussian_measured", 30, {'contrast': 4.0}],
+    ["schrodinger2D", "double_slit_measured", 30, {'contrast': 40.0}],
+    ["schrodinger2D", "convex_mirror", 30, {'contrast': 4.0}],
     ["classical_particle", "convex_mirror", 30],
-    ["schrodinger2D", "box_with_stuff", 30],
+    ["schrodinger2D", "box_with_stuff", 30, {'contrast': 10.0}],
 ]
 
 timestamps = [
@@ -70,41 +71,29 @@ for i in range(len(timestamps) - 1):
 
 resolution = "160p"
 width, height = RESOLUTIONS[resolution]
-sampling_multiplier = 1
-white_level = 64 * sampling_multiplier
-contrast = 16.0
+default_sampling_multiplier = 3.0
+white_level = 128
+default_contrast = 8.0
 fps = 60
 
 master_folder = tempfile.mkdtemp()
 frame = 0
 remainder = 0.0
 
-for episode in episodes:
+def render_episode(episode, folder, num_frames):
+    print(episode)
     if len(episode) == 3:
         command, episode, num_seconds = episode
         hide_phase = False
+        contrast = default_contrast
+        sampling_multiplier = default_sampling_multiplier
     elif len(episode) == 4:
-        command, episode, num_seconds, hide_phase = episode
+        command, episode, num_seconds, options = episode
+        hide_phase = options.get('hide_phase', False)
+        contrast = options.get('contrast', default_contrast)
+        sampling_multiplier = options.get('sampling_multiplier', default_sampling_multiplier)
 
-    folder = tempfile.mkdtemp()
-
-    num_frames = fps * num_seconds + remainder
-    remainder = num_frames - int(num_frames)
-    num_frames = int(num_frames)
-
-    if command == "text":
-        background = (100, 50, 30)
-        fontsize = int(height * 0.1)
-        font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf', fontsize)
-        image = Image.new('RGB', (width, height), background)
-        draw = ImageDraw.Draw(image)
-        for i, text in enumerate(episode.split('\n')):
-            w, h = font.getsize(text)
-            draw.text(((width-w)//2, int((height-h)*0.5 + 1.5*(i-0.5)*h)), text, fill='white', font=font)
-        for _ in range(num_frames):
-            image.save(os.path.join(master_folder, "frame{:05}.png".format(frame)))
-            frame += 1
-    elif "classical" in command:
+    if "classical" in command:
         subprocess.call(map(str, [
             "python", command + ".py", episode,
             "--folder", folder,
@@ -146,12 +135,45 @@ for episode in episodes:
     else:
         raise ValueError("Unknown command {}".format(command))
 
-    if command != "text":
-        for filename in sorted(os.listdir(os.path.join(folder, "png"))):
-            source = os.path.join(folder, "png", filename)
-            destination = os.path.join(master_folder, "frame{:05}.png".format(frame))
-            shutil.move(source, destination)
+threads = []
+folders = []
+for episode in episodes:
+    if len(episode) == 3:
+        command, arg, num_seconds = episode
+        hide_phase = False
+    elif len(episode) == 4:
+        command, arg, num_seconds, hide_phase = episode
+
+    folder = tempfile.mkdtemp()
+
+    num_frames = fps * num_seconds + remainder
+    remainder = num_frames - int(num_frames)
+    num_frames = int(num_frames)
+
+    if command == "text":
+        background = (100, 50, 30)
+        fontsize = int(height * 0.1)
+        font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf', fontsize)
+        image = Image.new('RGB', (width, height), background)
+        draw = ImageDraw.Draw(image)
+        for i, text in enumerate(arg.split('\n')):
+            w, h = font.getsize(text)
+            draw.text(((width-w)//2, int((height-h)*0.5 + 1.5*(i-0.5)*h)), text, fill='white', font=font)
+        for _ in range(num_frames):
+            image.save(os.path.join(master_folder, "frame{:05}.png".format(frame)))
             frame += 1
+    else:
+        folders.append(folder)
+        threads.append(Thread(target=render_episode, args=(episode, folder, num_frames)))
+        threads[-1].start()
+
+for folder, thread in zip(folders, threads):
+    thread.join()
+    for filename in sorted(os.listdir(os.path.join(folder, "png"))):
+        source = os.path.join(folder, "png", filename)
+        destination = os.path.join(master_folder, "frame{:05}.png".format(frame))
+        shutil.move(source, destination)
+        frame += 1
     shutil.rmtree(folder)
 
 os.chdir(master_folder)
